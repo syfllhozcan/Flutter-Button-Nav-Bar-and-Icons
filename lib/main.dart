149946434cf5ce5c8589329e4a68a264cfa8b7ea
syfllhozcan <syfllh.ozcan@gmail.com>
2023-07-24 01:28:59 +0300

    renkleri ayarladım hepsini f1f1f1 yaptım

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
-
 import 'package:button_nav_bar/ui/root.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 
 void main() async {
-  runApp( MyApp());
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+      
+    ),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        //initialRoute: AuthService().currentUser() != null ? FirstAddData() : Login(),
-        /*
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //initialRoute: AuthService().currentUser() != null ? FirstAddData() : Login(),
+      /*
            routes: {
           '/':(context) => RootApp(),
           '/firstAddComplaint':(context) => FirstAddData(),
@@ -28,8 +35,7 @@ class MyApp extends StatelessWidget {
 
         },
         */
-       home: RootApp(),
-      );
-    
+      home: RootApp(),
+    );
   }
 }
